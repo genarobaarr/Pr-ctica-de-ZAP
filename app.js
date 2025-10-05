bad93de7d89174c66b1e79667c9087241eccb793
genarobaarr <genaelcrack0409@gmail.com>
2025-10-04 23:59:21 -0600

    fix: Segundo commit con el código seguro

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
+const helmet = require('helmet'); //AGREGADO
 const db = require('./db');
 
 const app = express();
@@ -8,7 +9,9 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(helmet()); //AGREGADO
 
+/*
 // Ruta vulnerable a inyección SQL (intencional)
 app.get('/users', (req, res) => {
   const name = req.query.name || '';
@@ -18,11 +21,30 @@ app.get('/users', (req, res) => {
     res.render('users', { rows, q: name });
   });
 });
+*/
 
+// Ruta segura con consultas parametrizadas
+app.get('/users', (req, res) => {
+  const name = req.query.name || '';
+  const sql = "SELECT id, name, email FROM users WHERE name LIKE ?";
+  db.all(sql, [`%${name}%`], (err, rows) => {
+    if (err) return res.status(500).send('Error DB');
+    res.render('users', { rows, q: name });
+  });
+});
+
+/*
 // XSS reflejado (eco sin escapar)
 app.get('/echo', (req, res) => {
   const msg = req.query.msg || '';
   res.send(`<h1>Echo</h1><p>${msg}</p>`); // VULNERABLE
+});
+*/
+
+// views/echo.ejs (usa <%= msg %>)
+app.get('/echo', (req, res) => {
+  const msg = req.query.msg || '';
+  res.render('echo', { msg });
 });
 
 app.get('/', (req, res) => res.render('index'));
